28ce6daa33df193255c60c0d2cb5ac43b70a1099
Daniel J. Pinedo C <systemdany@gmail.com>
2022-10-03 22:50:40 -0400

    (develop-unit-test) Creacion del primer UnitTest de la aplicacion

---
+++ test/sandbox.test.js
@@ -0,0 +1,13 @@
+describe(
+    "Pruebas de la aplicacion: SoeBookApi",
+    () => {
+        test(
+            "Guardado de nuevo libro",
+            () => {
+                var result = false;
+
+                expect(result).toBe(true);
+            }
+        )
+    }
+);
